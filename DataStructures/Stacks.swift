7adbef71ea3e84a89cd6006186d3d9a9d4b63932
William Eze <eze.w@etoncollege.org.uk>
2024-02-27 12:20:00 +0000

    yup

--- DataStructures/Stacks.swift
+++ DataStructures/Stacks.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+
+class Stacks{
+    var stackPointer = -1
+    let max: Int
+    var contents: [Int]
+    
+    init(max: Int, contents: [Int]) {
+        self.max = max
+        self.contents = contents
+    }
+    
+    func push(data: Int){
+        // Check if full
+        if self.contents.count >= self.max{
+            print("Stack is full")
+        }
+        else{
+            self.contents.append(data)
+            self.stackPointer += 1
+        }
+    }
+    
+    func pop() {
+        if self.contents.count == 0{
+            print("Stack is empty")
+        }
+        else{
+            self.contents.remove(at: self.contents.count-1)
+        }
+    }
+    
+    func peek(){
+        if self.contents.count == 0{
+            print("Stack is empty")
+        }
+        else{
+            print(self.contents[0])
+        }
+    }
+
+    
+}
--- DataStructures/main.swift
+++ DataStructures/main.swift
@@ -7,5 +7,7 @@
 
 import Foundation
 
-print("Hello, World!")
-
+let eze = Stacks(max: 5, contents: [1,2,3,4,5])
+print(eze.contents)
+eze.pop()
+print(eze.contents)
